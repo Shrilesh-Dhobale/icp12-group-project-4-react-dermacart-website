2215e661e347781fec7e3766f854c14ef4832699
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-01-02 10:17:44 +0530

    React-Router imported

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter,Routes,Route } from 'react-router'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root=createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<h1>Home</h1>}/>
+  </Routes>
+  </BrowserRouter>
+);
