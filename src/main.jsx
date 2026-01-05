6596de77e67ca6249dfde202c61e64100ef1f9f5
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-01-05 10:46:20 +0530

    Products img added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter,Routes,Route } from 'react-router'
+import { BrowserRouter, Routes, Route } from 'react-router'
 
-const root=createRoot(document.getElementById("root"));
+
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<h1>Home</h1>}/>
-  </Routes>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+     
+    </Routes>
   </BrowserRouter>
 );
