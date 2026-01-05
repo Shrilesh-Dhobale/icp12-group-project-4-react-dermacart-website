c715db7cefd3aa75339104d2c93a71c74ebb4a78
OmKadam-1 <kom110798@gmail.com>
2026-01-05 16:45:59 +0530

    login function created

---
+++ src/views/Login.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
