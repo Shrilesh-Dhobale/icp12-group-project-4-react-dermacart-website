5e7ad71dea01b8aee586f65d93d0c7e375d2688b
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-01-08 15:39:34 +0530

    Products details added

---
+++ src/views/products.js
@@ -0,0 +1,100 @@
+const products=[
+  {
+    id: 1,
+    img:"../assets/com_prod(1).jpeg",
+    name:"DermaCart Soap",
+    price: 199,
+    description:"A gentle soap for sensitive skin.",
+    type:"Combination"
+  },
+  {
+    id: 2,
+    img:"../assets/com_prod(2).jpeg",
+    name:"DermaCart Face Mask",
+    price: 149,
+    description:"A hydrating face mask for combinational skin types.",
+    type:"Combination"
+  },
+  {
+    id: 3,
+    img:"../assets/com_prod(1).jpeg",
+    name:"Anti-Acne DermaCart Serum",
+    price: 459,
+    description:"A serum to combat acne and blemishes.",
+    type:"Combination"
+  },
+  {
+    id: 4,
+    img:"../assets/dry_prod(1).jpeg",
+    name:"DermaCart Moisturizer",
+    price: 249,
+    description:"A rich moisturizer for dry skin.",
+    type:"Dry"
+  },
+  {
+    id: 5,
+    img:"../assets/dry_prod(2).jpeg",
+    name:"DermaCart Natural Lip Balm",
+    price: 99,
+    description:"A nourishing lip balm for dry lips.",
+    type:"Dry"
+  },
+  {
+    id: 6,
+    img:"../assets/dry_prod(3).jpeg",
+    name:"DermaCart Moisturizing Body Wash",
+    price: 189,
+    description:"A body wash that hydrates and soothes dry skin.",
+    type:"Dry"
+  },
+  {
+    id: 7,
+    img:"../assets/nor_prod(1).jpeg",
+    name:"DermaCart Sunscreen",
+    price: 249,
+    description:"A lightweight sunscreen for normal skin.",
+    type:"Dry"
+  },
+  {
+    id: 8,
+    img:"../assets/nor_prod(2).jpeg",
+    name:"DermaCart Body Wash",
+    price: 159,
+    description:"A gentle body wash for normal skin.",
+    type:"Normal"
+  },
+  {
+    id: 9,
+    img:"../assets/nor_prod(3).jpeg",
+    name:"DermaCart Vitamin C Serum",
+    price: 399,
+    description:"A serum to brighten and even out skin tone.",
+    type:"Normal"
+  },
+  {
+    id: 10,
+    img:"../assets/oily_prod(1).jpeg",
+    name:"DermaCart Foaming Face Wash",
+    price: 499,
+    description:"A foaming face wash for oily skin.",
+    type:"Oily"
+  },
+  {
+    id: 11,
+    img:"../assets/oily_prod(2).jpeg",
+    name:"DermaCart Body Wash",
+    price: 299,
+    description:"A body wash for oily skin.",
+    type:"Oily"
+  },
+  {
+    id: 12,
+    img:"../assets/oily_prod(3).jpeg",
+    name:"DermaCart UnderEye Cream",
+    price: 349,
+    description:"An under-eye cream to reduce puffiness and dark circles.",
+    type:"Oily"
+  }
+];
+
+export default products;
