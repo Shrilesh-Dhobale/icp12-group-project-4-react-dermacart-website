8e233cd4c5c35ff13c44120b5847c12b8f52d2a5
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-01-08 20:42:14 +0530

    products imported

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import products from '../views/products.js'
+import {BG_color,Hero_BG_color,Primary_color,Main_Text_color,Sub_Headers_color}from '../views/app.js'
 
-function ProductCard({name, img, price, description}) {
+function ProductCard({products}) {
   return (
     
-        <div key={products.id} className="bg-white rounded-lg shadow-md p-4 w-64">
-          <img src={products.img} alt={products.name} className="w-full h-58 object-cover rounded-md mb-2" />
+        <div key={products.id} className="bg-white rounded-lg shadow-md p-4 w-84">
+          <img src={products.img} alt={products.name} className="w-full h-68 object-cover rounded-md mb-2" />
           <h3 className="text-lg font-semibold">{products.name}</h3>
           <p className="text-gray-600">{products.description}</p>
           <p className="text-green-600 font-bold">₹{products.price}</p>
---
+++ src/views/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import products from '../views/products.js'
+
+function Products() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Products
