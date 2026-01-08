3f473cab7f1d114456e1c1f7b2146a78f4778b24
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-01-08 20:42:51 +0530

    Colors imported

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import products from '../views/products.js'
+import {BG_color,Hero_BG_color,Primary_color,Main_Text_color,Sub_Headers_color}from '../views/app.js'
 
 function Products() {
   return (
