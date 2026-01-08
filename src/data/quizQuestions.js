01745cd23b31291b5d68f0b06de5be5376bf2328
kalyanimurkute <murkutekalyani259@gmail.com>
2026-01-08 13:12:53 +0530

    add skin related questions

---
+++ src/data/quizQuestions.js
@@ -0,0 +1,29 @@
+const quizData = [
+  {
+    id: 1,
+    question: "How does your skin feel a few hours after washing your face?",
+    options: [
+      {
+        id: "oily",
+        label: "Greasy / Shiny",
+        image: "./../public/shiny1.jpg",
+      },
+      {
+        id: "dry",
+        label: "Tight / Dry",
+        image: "./../public/dry.jpg",
+      },
+      {
+        id: "normal",
+        label: "Comfortable / Soft",
+        image: "./../public/normal.jpg",
+      },
+      {
+        id: "combination",
+        label: "Oily in some areas, dry in others",
+        image: "./../public/combination.jpg",
+      },
+    ],
+  },
+];
+  export default quizData;
