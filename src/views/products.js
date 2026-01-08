3d9e73774cfc487566a2e563a3ac4f018dbfef75
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-01-08 13:11:12 +0530

    Product added for dry and combinational skin type

--- src/views/products.js
+++ src/views/products.js
@@ -1,1 +1,194 @@
-
+const products=[
+    {
+        id:1,
+        img:"/assets/com_facewash(1).jpg",
+        name:"Tea Tree FaceWash",
+        product:"Facewash",
+        description:"Skin Clearing Facial Wash",
+        price:$199
+    },
+    {
+        id:2,
+        img:"/assets/com_facewash(2).jpg",
+        name:"Cica Gentle Face Cleanser",
+        product:"Facewash",
+        description:"Powerful blend of Jojoba oil and vitamin E",
+        price:$259
+    },
+     {
+        id:3,
+        img:"/assets/com_facewash(3).webp",
+        name:"Mango Detan Gel Face Wash",
+        product:"Facewash",
+        description:"Detan Gel Face Wash with Mango Extracts",
+        price:$219
+    },
+     {
+        id:4,
+        img:"/assets/com_facewash(4).jpg",
+        name:"Niacinamide Face Wash",
+        product:"Facewash",
+        description:"Niacinamide & Zinc PCA",
+        price:$299
+    },
+    {
+        id:5,
+        img:"/assets/com_moist(1).jpg",
+        name:"Derma Moisturizer",
+        product:"Moisturizer",
+        description:"Hydrating Face Moisturizer with Hyaluronic Acid",
+        price:$319
+    },
+    {
+        id:6,
+        img:"/assets/com_moist(2).jpg",
+        name:"Vitamin B5 Moisturizer",
+        product:"Moisturizer",
+        description:"Lightweight Moisturizer with Vitamin B5",
+        price:$249
+    },
+    {
+        id:7,
+        img:"/assets/com_moist(3).webp",
+        name:"Morning Nectar Moisturizer",
+        product:"Moisturizer",
+        description:"Nourishing Moisturizer with Honey Extracts",
+        price:$379
+    },
+    {
+        id:8,
+        img:"/assets/com_moist(4).webp",
+        name:"Hydrating Light Moisturizer",
+        product:"Moisturizer",
+        description:"Oil-Free Hydrating Light Moisturizer",
+        price:$339
+    },
+    {
+        id:9,
+        img:"/assets/com_sun(1).webp",
+        name:"Sun Miracle ",
+        product:"Sunscreen",
+        description:"SPF 50 PA+++",
+        price:$309
+    },
+     {
+        id:10,
+        img:"/assets/com_sun(2).webp",
+        name:"Matte Sunscreen",
+        product:"Sunscreen",
+        description:"SPF 50 PA+++",
+        price:$279
+    },
+     {
+        id:11,
+        img:"/assets/com_sun(3).avif",
+        name:"Wqlder Sunscreen",
+        product:"Sunscreen",
+        description:"SPF 50 PA+++",
+        price:$289
+    },
+    {
+        id:12,
+        img:"/assets/com_sun(4).jpg",
+        name:"Minimalist Sunscreen",
+        product:"Sunscreen",
+        description:"Light Fluid Sunscreen SPF 50 ",
+        price:$299
+    }, 
+    {
+        id:13,
+        img:"/assets/dry_facewash(1).webp",
+        name:"Soothing Foam Face Wash",
+        product:"Facewash",
+        description:"Gently Cleansing Face Wash for Dry Skin",
+        price:$399
+    },
+    {
+        id:14,
+        img:"/assets/dry_facewash(2).avif",
+        name:"Ziaja Foam Face Wash",
+        product:"Facewash",
+        description:"Cleansing Foam Face Wash with Pro-Vitamin B5",
+        price:$319
+    },  
+    {
+        id:15,
+        img:"/assets/dry_facewash(3).jpg",
+        name:"Jojoba Wash",
+        product:"Facewash",
+        description:"Active Milli capsules with Jojoba Oil",
+        price:$319
+    },
+    {
+        id:16,
+        img:"/assets/dry_facewash(4).webp",
+        name:"Plum Face Wash",
+        product:"Facewash",
+        description:"Saffron & Papaya Enriched Face Wash",
+        price:$399
+    },
+    {
+        id:17,
+        img:"/assets/dry_moist(1).jpg",
+        name:"Hyaluron Moisturizer",
+        product:"Moisturizer",
+        description:"Hydrating Moisturizer with Hyaluronic Acid",
+        price:$389
+    },
+    {
+        id:18,
+        img:"/assets/dry_moist(2).jpg",
+        name:"Plix Moisturizer",
+        product:"Moisturizer",
+        description:"Nourishing Moisturizer with Almond Oil",
+        price:$289
+    },
+    {
+        id:19,
+        img:"/assets/dry_moist(3).jpg",
+        name:"Eucerin Moisturizer",
+        product:"Moisturizer",
+        description:"Rich Moisturizer for Dry Skin",
+        price:$209
+    },
+    {
+        id:20,
+        img:"/assets/dry_moist(4).webp",
+        name:"The Ordinary Moisturizer",
+        product:"Moisturizer",
+        description:"Moisturizer with Squalane",
+        price:$389
+    },
+    {
+        id:21,
+        img:"/assets/dry_sun(1).webp",
+        name:"HydraGel India Sunscreen",
+        product:"Sunscreen",
+        description:"Aleo Vera and Raspberry Extracts",
+        price:$289
+    },
+    {
+        id:22,
+        img:"/assets/dry_sun(2).webp",
+        name:"Cetaphil Sunscreen",
+        product:"Sunscreen",
+        description:"Light Gel Sunscreen SPF 50",
+        price:$349
+    },
+    {
+        id:23,
+        img:"/assets/dry_sun(3).jpg",
+        name:"After sun Sunscreen",
+        product:"Sunscreen",
+        description:"Hydrating and Claming Face cream",
+        price:$359
+    },
+    {
+        id:24,
+        img:"/assets/dry_sun(4).webp",
+        name:"Lakme 9 to 5 Sunscreen",
+        product:"Sunscreen",
+        description:"Sun Expert Delight Matte SPF 50 PA+++",
+        price:$269
+    },
+];
