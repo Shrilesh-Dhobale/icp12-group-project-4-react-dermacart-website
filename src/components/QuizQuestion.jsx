2d92216dcfd60d8f171a628725ca4add862cc21b
kalyanimurkute <murkutekalyani259@gmail.com>
2026-01-08 13:16:19 +0530

    add quiz option

--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-function QuizQuestion() {
+ function QuizOption({ image, text, onClick, selected = false }) {
   return (
-    <div>QuizQuestion</div>
-  )
+    <div
+      onClick={onClick}
+      className={`flex items-center gap-3 border rounded-xl px-3 py-2 sm:px-4 sm:py-3 bg-white/40 backdrop-blur-sm cursor-pointer transition w-full ${
+        selected ? "border-pink-400 bg-pink-50": "border-gray-300 hover:border-pink-300 hover:bg-pink-50/40"
+      }`}
+      role="button"
+      tabIndex={0}
+    >
+      <img
+        src={image}
+        alt={text}
+        className="w-7 h-7 sm:w-10 sm:h-10 rounded-full object-cover"
+      />
+      <span className="text-black-800 font-medium text-xs sm:text-sm">{text}</span>
+    </div>
+  );
 }
-
-export default QuizQuestion
+export default QuizOption;
