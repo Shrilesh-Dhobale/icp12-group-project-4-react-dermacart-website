2fbaaa765153c4ee3754db83332a67dfd9ef3d49
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-01-08 20:46:50 +0530

    Map function applied on cards

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -5,8 +5,12 @@ import ProductCard from '../components/ProductCard.jsx'
 
 function Products() {
   return (
-    <div>
-      
+    <div className="{{style=BG_color}} min-h-screen p-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {products.map((product) => (
+          <ProductCard key={product.id} products={product} />
+        ))}
+      </div>
     </div>
   )
 }
