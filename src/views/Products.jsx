e06fe679d2cda5a67bac0d5ddd965f0610fea69c
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2026-01-09 10:50:56 +0530

    Search input added

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -7,6 +7,13 @@ import ProductCard from '../components/ProductCard.jsx'
 function Products() {
   return (
     <div className="min-h-screen py-8" style={{ backgroundColor: BG_color }}>
+      <div className='flex flex-row-reverse p-4'>
+        <input
+          type="text"
+          placeholder="Search products..."
+          className="w-1/6 p-2 mb-6 rounded-md border border-gray-300 focus:outline-none focus:ring-1"
+        />
+      </div>
       <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
